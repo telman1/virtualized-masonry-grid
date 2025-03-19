aaa69f36e58e2a0681c63cda04b7f095bb8dd12a
telmansargsyan <sargsyan.telman2@gmail.com>
2025-03-19 16:52:17 +0400

    Setup boilerplate project with dummy masonry grid

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GridLayout from "./components/gridLayout/GridLayout.tsx";
+
+function App() {
+
+  return (
+    <>
+      <GridLayout/>
+    </>
+  )
+}
+
+export default App
