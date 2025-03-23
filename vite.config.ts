1be574e806c724346028278e070cc4f17a65eeba
telmansargsyan <sargsyan.telman2@gmail.com>
2025-03-23 21:10:56 +0400

    Fixed Pexels API issue by patching isomorphic-fetch and ensuring compatibility with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,39 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
+import commonjs from "vite-plugin-commonjs";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
-    visualizer({
-      filename: "bundle-report.html",
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
+    ...(mode === "development"
+      ? [
+        visualizer({
+          filename: "bundle-report.html",
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+        }),
+      ]
+      : []),
+    commonjs(),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      "isomorphic-fetch": "cross-fetch",
     },
   },
-  build: {
-    sourcemap: true,
+  define: {
+    global: "window",
   },
-});
-
+  build: {
+    // sourcemap: true,
+    rollupOptions: {
+      external: ["isomorphic-fetch"],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+}));
